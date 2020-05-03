3d5666dde34609b7c715b17ec25e3991581f9b9c
loc.nguyenvu <loc.nguyenvu@tiki.vn>
2020-05-03 14:07:56 +0700

    Add filter for item extra

--- src/tikivn/Oms/Order/Model/Item.php
+++ src/tikivn/Oms/Order/Model/Item.php
@@ -3,5 +3,11 @@ namespace Tikivn\Oms\Order\Model;
 
 class Item extends \Carrot\Common\Model
 {
-    
+
+    public function assign($data) {
+        $extra = new Item\Extra($data['extra'] ?? '');
+
+        $data['extra'] = $extra;
+        parent::assign($data);
+    }
 }
---
+++ src/tikivn/Oms/Order/Model/Item/Extra.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tikivn\Oms\Order\Model\Item;
+
+class Extra extends \Carrot\Common\Model
+{
+    public function __construct(string $extraString = null) {
+        if ($extraString == null) {
+            return;
+        }
+
+        $extraData = json_decode(\stripslashes($extraString), true);
+        if (JSON_ERROR_NONE == \json_last_error()) {
+            $this->_properties = $extraData;
+        }
+    }
+}
